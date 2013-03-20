c00bb3448998c48ed66b640c37de0475e62df616
Masafumi Yokoyama <myokoym@gmail.com>
2013-03-20 14:55:07 +0900

    Extract method

--- lib/rabbit_viewer/command/rabbit_viewer.rb
+++ lib/rabbit_viewer/command/rabbit_viewer.rb
@@ -17,25 +17,32 @@ module RabbitViewer
 
       def run(arguments)
         Tempfile.open(["rabbit_viewer", ".rab"]) do |tempfile|
-          title = <<-EOT
-= RabbitViewer
-: date
-   version #{::RabbitViewer::VERSION}
-          EOT
           tempfile.puts(title)
         
           arguments.each do |viewfile|
-            page = <<-EOT
-= #{File.basename(viewfile)}
-  # image
-  # src = #{viewfile}
-            EOT
-            tempfile.puts(page)
+            tempfile.puts(page(viewfile))
           end
         
           tempfile.flush
           Rabbit::Command::Rabbit.run(tempfile.path)
         end
+      end
+
+      private
+      def title
+        <<-EOT
+= RabbitViewer
+: date
+   version #{::RabbitViewer::VERSION}
+        EOT
+      end
+
+      def page(viewfile)
+        <<-EOT
+= #{File.basename(viewfile)}
+  # image
+  # src = #{viewfile}
+        EOT
       end
     end
   end
