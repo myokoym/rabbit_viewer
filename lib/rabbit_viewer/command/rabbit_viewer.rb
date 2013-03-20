79af13c11df725fe3a5e080861ce9e044dcb2a13
Masafumi Yokoyama <myokoym@gmail.com>
2013-03-20 11:42:27 +0900

    Move shell command code from bin to lib

---
+++ lib/rabbit_viewer/command/rabbit_viewer.rb
@@ -0,0 +1,42 @@
+require "rabbit/command/rabbit"
+require "fileutils"
+
+module RabbitViewer
+  module Command
+    class RabbitViewer
+
+      class << self
+        def run(*arguments)
+          new.run(arguments)
+        end
+      end
+
+      def initialize
+      end
+
+      def run(arguments)
+        tempfile= "/tmp/rabbit-viewer.#{$$}.tmp.rab"
+        
+        begin
+          File.open(tempfile, "a") do |tempfile|
+            title = "= rabbit-viewer"
+            tempfile.puts(title)
+        
+            Dir.glob(arguments[0]) do |viewfile|
+              page = <<-EOT
+= #{File.basename(viewfile)}
+  # image
+  # src = #{viewfile}
+              EOT
+              tempfile.puts(page)
+            end
+          end
+        
+          Rabbit::Command::Rabbit.run(tempfile)
+        ensure
+          FileUtils.rm(tempfile)
+        end
+      end
+    end
+  end
+end
